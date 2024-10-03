e59e4b2f3712e9263a172bcad133e8056829b1dc
chdineshbabu <chdinesh4128@gmail.com>
2024-10-04 02:10:40 +0530

    amo

--- components/Token.tsx
+++ components/Token.tsx
@@ -10,18 +10,20 @@ import {
   getMintLen,
   LENGTH_SIZE,
   TOKEN_2022_PROGRAM_ID,
-  TOKEN_PROGRAM_ID,
   TYPE_SIZE,
 } from "@solana/spl-token";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 import { createInitializeInstruction, pack } from "@solana/spl-token-metadata";
 import { Atom } from "react-loading-indicators";
-
 import { useState } from "react";
 import { CircleAlert, CircleCheckBig, X } from "lucide-react";
 
-export default function Token({ onClose }) {
+interface TokenProp {
+  onClose: () => void;
+}
+
+export default function Token({ onClose }: TokenProp): JSX.Element {
   const [status, setStatus] = useState("Creating token");
   const [loader, setLoader] = useState("load");
   const [tokenName, setTokenName] = useState("");
@@ -31,11 +33,13 @@ export default function Token({ onClose }) {
   const [isCreating, setIsCreating] = useState(false);
   const { wallet } = useWallet();
   const { connection } = useConnection();
-  if (!wallet?.adapter.publicKey) {
-  console.error("Wallet is not connected or publicKey is not available.");
-  return;
-}
+
   async function createToken() {
+    if (!wallet || !wallet.adapter.publicKey) {
+      console.error("Wallet is not connected or publicKey is not available.");
+      return;
+    }
+
     setStatus("Creating Mint Account...");
     const mintKeypair = Keypair.generate();
     const metadata = {
@@ -46,15 +50,17 @@ export default function Token({ onClose }) {
       additionalMetadata: [],
     };
     console.log(mintKeypair.publicKey, metadata);
+
     const mintLen = getMintLen([ExtensionType.MetadataPointer]);
     const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
 
     const lamports = await connection.getMinimumBalanceForRentExemption(
       mintLen + metadataLen
     );
+
     const transaction = new Transaction().add(
       SystemProgram.createAccount({
-        fromPubkey: wallet?.adapter.publicKey,
+        fromPubkey: wallet.adapter.publicKey, // Use the publicKey directly here
         newAccountPubkey: mintKeypair.publicKey,
         space: mintLen,
         lamports,
@@ -62,14 +68,14 @@ export default function Token({ onClose }) {
       }),
       createInitializeMetadataPointerInstruction(
         mintKeypair.publicKey,
-        wallet?.adapter.publicKey,
+        wallet.adapter.publicKey,
         mintKeypair.publicKey,
         TOKEN_2022_PROGRAM_ID
       ),
       createInitializeMintInstruction(
         mintKeypair.publicKey,
         9,
-        wallet?.adapter.publicKey,
+        wallet.adapter.publicKey,
         null,
         TOKEN_2022_PROGRAM_ID
       ),
@@ -80,57 +86,64 @@ export default function Token({ onClose }) {
         name: metadata.name,
         symbol: metadata.symbol,
         uri: metadata.uri,
-        mintAuthority: wallet?.adapter.publicKey,
-        updateAuthority: wallet?.adapter.publicKey,
+        mintAuthority: wallet.adapter.publicKey,
+        updateAuthority: wallet.adapter.publicKey,
       })
     );
+
     setStatus("Creating Token.....");
-    transaction.feePayer = wallet?.adapter.publicKey;
+    transaction.feePayer = wallet.adapter.publicKey;
     transaction.recentBlockhash = (
       await connection.getLatestBlockhash()
     ).blockhash;
     transaction.partialSign(mintKeypair);
 
-    await wallet?.adapter.sendTransaction(transaction, connection);
+    await wallet.adapter.sendTransaction(transaction, connection);
     setStatus("Associating the Account.....");
+
     const associatedTokenAccount = await getAssociatedTokenAddress(
       mintKeypair.publicKey,
-      wallet?.adapter.publicKey,
+      wallet.adapter.publicKey,
       false,
       TOKEN_2022_PROGRAM_ID
     );
+
     const associateTransaction = new Transaction().add(
       createAssociatedTokenAccountInstruction(
-        wallet?.adapter.publicKey,
+        wallet.adapter.publicKey,
         associatedTokenAccount,
-        wallet?.adapter.publicKey,
+        wallet.adapter.publicKey,
         mintKeypair.publicKey,
         TOKEN_2022_PROGRAM_ID
       )
     );
-    transaction.feePayer = wallet?.adapter.publicKey;
-    transaction.recentBlockhash = (
+
+    associateTransaction.feePayer = wallet.adapter.publicKey;
+    associateTransaction.recentBlockhash = (
       await connection.getLatestBlockhash()
     ).blockhash;
-    await wallet?.adapter.sendTransaction(associateTransaction, connection);
+    await wallet.adapter.sendTransaction(associateTransaction, connection);
+
     setStatus("Successfully Token Created");
     const instruction = createMintToInstruction(
       mintKeypair.publicKey,
       associatedTokenAccount,
-      wallet?.adapter.publicKey,
-      initialSupply * 1000000000,
+      wallet.adapter.publicKey,
+      Number(initialSupply) * 1000000000, // Convert initialSupply to a number
       [],
       TOKEN_2022_PROGRAM_ID
     );
+
     setStatus("Minting the Tokens.....");
     const tokenMintTransaction = new Transaction().add(instruction);
-    await wallet?.adapter.sendTransaction(tokenMintTransaction, connection);
+    await wallet.adapter.sendTransaction(tokenMintTransaction, connection);
 
     console.log(`Token created: ${mintKeypair.publicKey.toBase58()}`);
     console.log(
       `Associated Token Account: ${associatedTokenAccount.toBase58()}`
     );
   }
+
   console.log(status);
   const handleCreateToken = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -138,14 +151,13 @@ export default function Token({ onClose }) {
     await createToken();
     setLoader("success");
   };
-  
+
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-50">
       {isCreating ? (
         <div className="lg:w-[40%] w-[50%] rounded-lg bg-black text-white dark:bg-white dark:text-black p-16">
           <div className="flex justify-between">
             <div></div>
-
             <button
               onClick={onClose}
               className="text-gray-500 hover:text-gray-100 dark:hover:text-black"
@@ -154,14 +166,14 @@ export default function Token({ onClose }) {
             </button>
           </div>
           <div className="flex justify-center">
-            {loader == "success" ? (
+            {loader === "success" ? (
               <div className="flex flex-col items-center gap-5 justify-center">
                 <CircleCheckBig className="w-28 h-28 text-green-700" />
                 <h1 className="font-poppins text-xl text-green-800">
                   Created Successfully
                 </h1>
               </div>
-            ) : loader == "fail" ? (
+            ) : loader === "fail" ? (
               <div className="flex flex-col items-center gap-5 justify-center">
                 <CircleAlert className="w-28 h-28 text-red-700" />
                 <h1 className="font-poppins text-xl text-red-800">
@@ -186,7 +198,7 @@ export default function Token({ onClose }) {
               <X />
             </button>
           </div>
-          <p className=" mb-4">Enter the details for your new token</p>
+          <p className="mb-4">Enter the details for your new token</p>
           <form onSubmit={handleCreateToken}>
             <div className="space-y-4">
               <div>
@@ -243,30 +255,27 @@ export default function Token({ onClose }) {
               <div>
                 <label
                   htmlFor="initialSupply"
-                  className="block text-sm font-medium "
+                  className="block text-sm font-medium"
                 >
                   Initial Supply
                 </label>
                 <input
                   id="initialSupply"
                   type="number"
-                  placeholder="000000"
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm text-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  min="0"
+                  placeholder="1000"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 text-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   value={initialSupply}
                   onChange={(e) => setInitialSupply(e.target.value)}
                   required
                 />
               </div>
-            </div>
 
-            <div className="mt-6">
               <button
                 type="submit"
-                className={`w-full px-4 py-2 font-bold dark:bg-black bg-white dark:text-white text-black rounded hover:scale-105 transition-all delay-75 ${isCreating ? "opacity-50 cursor-not-allowed" : ""
-                  }`}
-                disabled={isCreating}
+                className="w-full inline-flex justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
-                {isCreating ? "Creating..." : "Create Token"}
+                Create Token
               </button>
             </div>
           </form>
